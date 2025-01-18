ee3ac850d4715de7d051ed268f07988d5aaf3e46
MohaEttaibi <mohammed.ettayby@gmail.com>
2025-01-18 19:02:53 +0100

    Create & Get Posts

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from models.post import UserPost, UserPostIn
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Welcome To Fast API."}
+# @app.get("/")
+# async def root():
+#     return {"message": "Welcome To Fast API."}
+
+post_table = {}
+
+@app.post("/post", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.dict()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+
+@app.get("/post", response_model=list[UserPost])
+async def get_all_post():
+    return list(post_table.values())
