04735046cb7d6950b3f7f775a44631f74166fdd4
MohaEttaibi <mohammed.ettayby@gmail.com>
2025-01-21 16:31:52 +0100

    continuing testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,25 +3,25 @@ from fastapi.testclient import TestClient
 from httpx import AsyncClient
 import pytest
 
-from main import app
+from store.main import app
 from routers.post import comment_table, post_table
 
 @pytest.fixture(scope="session")
 def anyio_backend():
     return "asyncio"
 
-
 @pytest.fixture()
 def client() -> Generator:
-    yield TestClient(app)
+    with TestClient(app) as client:
+        yield client
 
-@pytest.fixture(autuse=True)
+@pytest.fixture(autouse=True)
 async def db() -> AsyncGenerator:
     post_table.clear()
     comment_table.clear()
     yield
 
 @pytest.fixture()
-async def async_client(client) -> AsyncGenerator:
-    async with AsyncClient(app=app, base_url=client.base_url) as ac:
+async def async_client() -> AsyncGenerator:
+    async with AsyncClient(base_url="http://127.0.0.1:8000") as ac:
         yield ac
